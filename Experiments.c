86c026ab24a9d0494f279f941a667ebb23b1a659
Yatendra Singh <yatendra.singh@coforge.com>
2023-05-20 21:35:58 +0530

    Initial commit

---
+++ Experiments.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+void playWithCommandLineArgs(int argc, char *argv[]) {
+	for ( int i = 0 ; i < argc ; i++ ) {
+		printf("\n Argument Value At %d = %s", i, argv[i] );
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+	printf("\n\nFunction : playWithCommandLineArgs");
+	playWithCommandLineArgs( argc, argv );
+
+
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");
+	// printf("\n\nFunction : ");	
+}
+
